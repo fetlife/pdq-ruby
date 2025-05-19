4593e499b3ac005e4403d7d49898daca8fec8045
Pez Cuckow <email@pezcuckow.com>
2025-05-19 17:02:46 +0200

    🎉 Initial commit

---
+++ lib/pdq.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "pdq/version"
+require_relative "pdq/pdq"
+
+module Pdq
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/pdq_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pdq do
+  it "has a version number" do
+    expect(Pdq::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
